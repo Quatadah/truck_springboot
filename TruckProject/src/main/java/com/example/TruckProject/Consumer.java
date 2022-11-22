c91bdce091b351c02cec098b130042250e60ccad
NASDAMI Quatadah <quatadah.nasdami@gmail.com>
2022-11-22 10:06:12 +0100

    Get Last hour messages from kafka

--- TruckProject/src/main/java/com/example/TruckProject/Consumer.java
+++ TruckProject/src/main/java/com/example/TruckProject/Consumer.java
@@ -1,7 +1,11 @@
 package com.example.TruckProject;
 
+import java.util.Map;
+
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware.ConsumerSeekCallback;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +15,10 @@ public class Consumer {
     private static final String TOPIC = "trucks.position";
 
     @Autowired ServiceTruck serviceTruck;
+    
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {        
+        callback.seekToTimestamp(assignments.keySet(), System.currentTimeMillis() - 60 * 60 * 1000);
+    }
 
     @KafkaListener(id="h-q", topics=TOPIC)
     public Truck receive(Truck truck, Acknowledgment ack){
